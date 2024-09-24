09dbee1c02d5f77072db6cb0340031a91d2d694d
Joseph Platta <jwplatta@gmail.com>
2024-09-23 23:29:20 -0500

    feat: open file after creating a note from a chat

--- src/chat/AssistantMessage.tsx
+++ src/chat/AssistantMessage.tsx
@@ -1,49 +1,80 @@
-import { FC } from 'react';
+import { FC, useRef, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Copy, FilePlus } from 'lucide-react';
-import { App } from 'obsidian';
+import { App, ItemView, MarkdownRenderer, htmlToMarkdown } from 'obsidian';
 
 interface AssistantMessageProps {
   app: App;
   role: string;
   content: string;
+  chatView: ItemView;
 }
 
-export const AssistantMessage: FC<AssistantMessageProps> = ({ app, role, content }) => {
+export const AssistantMessage: FC<AssistantMessageProps> = ({ app, role, content, chatView }) => {
+  const messageContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (messageContainerRef.current) {
+      messageContainerRef.current.innerHTML = '';
+
+      console.log("Rendering Assistant message:\n", content);
+
+      MarkdownRenderer.render(
+        app,
+        content,
+        messageContainerRef.current,
+        app.vault.getRoot().path,
+        chatView
+      );
+    }
+  }, [content, chatView]);
+
   const copyToClipboard = () => {
     if (content) {
       navigator.clipboard.writeText(content).then(() => {
+        // noop
       }).catch((err) => {
         console.error("Failed to copy: ", err);
       });
     }
   };
 
-  const createNewNote = () => {
+  // const renderMessage = () => {
+  //   return htmlToMarkdown(
+  //     "<div>" + content + "</div>",
+  //   )
+  // }
+
+  const createNewNote = async () => {
     const defaultNewFileLocation = (app.vault as any).getConfig('newFileLocation');
     const title = `ChatNote-${Date.now()}.md`;
     let path = "";
 
     if (defaultNewFileLocation === 'root') {
-      path = "/" + title;
+      path = title;
     } else if (defaultNewFileLocation === 'current') {
       const activeFile = app.workspace.getActiveFile();
       if (!activeFile) {
-        path = "/" + title;
+        path = title;
       } else {
-        path = (activeFile.path || "/") + title;
+        path = (activeFile.path || "") + title;
       }
     } else {
-      const dir = (app.vault as any).getConfig('newFileFolderPath') + "/"
+      const dir = (app.vault as any).getConfig('newFileFolderPath')
       path = dir + title;
     }
+    await app.vault.create(path, content);
+    const f = await app.vault.getFileByPath(path);
 
-    app.vault.create(path, content);
+    if (f) {
+      const leaf = app.workspace.getLeaf();
+      await leaf.openFile(f);
+    }
   }
 
   return (
     <div className="chat-assistant-message">
-      <ReactMarkdown>{content || ""}</ReactMarkdown>
+      <div ref={messageContainerRef} />
       <div className="chat-assistant-controls">
         <div className="chat-assistant-control tool-tip" data-tooltip="Copy to clipboard" onClick={copyToClipboard}>
           <Copy size={20} />
