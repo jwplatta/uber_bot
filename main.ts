43ad63e1f234df773109c7f44eccbb9ad6b09306
Joseph Platta <jwplatta@gmail.com>
2024-10-12 15:46:49 -0500

    refactor: move the search assistant modal to its own file

--- main.ts
+++ main.ts
@@ -14,6 +14,7 @@ import { chatHistorySettings } from '@/src/settings/ChatHistorySettings';
 import { openAISettings } from '@/src/settings/OpenAISettings';
 import { ollamaSettings } from '@/src/settings/OllamaSettings';
 import AssistantFormModal from '@/src/AssistantFormModal';
+import SearchAssistantModal from '@/src/SearchAssistantModal';
 
 
 export interface NoteSecretarySettings {
@@ -362,49 +363,6 @@ class SelectEditAssistanModal extends SuggestModal<TFile> {
 
 	onChooseSuggestion(assistantFile: TFile, evt: MouseEvent | KeyboardEvent) {
 		new AssistantFormModal(this.app, this.settings, assistantFile).open();
-	}
-}
-
-class SearchAssistantModal extends SuggestModal<TFile> {
-	app: App;
-	settings: NoteSecretarySettings;
-	plugin: NoteSecretary;
-	chatWithNote: boolean;
-	assistantFiles: TFile[];
-
-	constructor(app: App, settings: NoteSecretarySettings, plugin: NoteSecretary, chatWithNote = false) {
-		super(app);
-		this.app = app;
-		this.settings = settings;
-		this.plugin = plugin;
-		this.chatWithNote = chatWithNote;
-
-		const files = this.app.vault.getMarkdownFiles();
-		this.assistantFiles = files.filter((file: TFile) => {
-			return file.path.includes(this.settings.assistants.assistantDefinitionsPath);
-		})
-	}
-
-	async getSuggestions(query: string): Promise<TFile[]> {
-		const filteredAssistants = this.assistantFiles.filter((assistant) => {
-			return assistant.basename.toLowerCase().includes(query.toLowerCase());
-		});
-
-		return filteredAssistants;
-	}
-
-	renderSuggestion(assistantFile: TFile, el: HTMLElement) {
-		el.createEl('h4', { text: assistantFile.basename, cls: 'assistant-suggestion-header' });
-		el.createEl('h6', { text: assistantFile.path, cls: 'assistant-suggestion-path' });
-	}
-
-	onChooseSuggestion(assistantFile: TFile, evt: MouseEvent | KeyboardEvent) {
-		if (this.chatWithNote) {
-			const activeFile = this.app.workspace.getActiveFile();
-			this.plugin.startNewChat(assistantFile, activeFile);
-		} else {
-			this.plugin.startNewChat(assistantFile, null);
-		}
 	}
 }
 
---
+++ src/SearchAssistantModal.ts
@@ -0,0 +1,46 @@
+import { App, TFile, SuggestModal } from 'obsidian';
+import NoteSecretary from '@/main'
+import { NoteSecretarySettings } from '@/main';
+
+export default class SearchAssistantModal extends SuggestModal<TFile> {
+	app: App;
+	settings: NoteSecretarySettings;
+	plugin: NoteSecretary;
+	chatWithNote: boolean;
+	assistantFiles: TFile[];
+
+	constructor(app: App, settings: NoteSecretarySettings, plugin: NoteSecretary, chatWithNote = false) {
+		super(app);
+		this.app = app;
+		this.settings = settings;
+		this.plugin = plugin;
+		this.chatWithNote = chatWithNote;
+
+		const files = this.app.vault.getMarkdownFiles();
+		this.assistantFiles = files.filter((file: TFile) => {
+			return file.path.includes(this.settings.assistants.assistantDefinitionsPath);
+		})
+	}
+
+	async getSuggestions(query: string): Promise<TFile[]> {
+		const filteredAssistants = this.assistantFiles.filter((assistant) => {
+			return assistant.basename.toLowerCase().includes(query.toLowerCase());
+		});
+
+		return filteredAssistants;
+	}
+
+	renderSuggestion(assistantFile: TFile, el: HTMLElement) {
+		el.createEl('h4', { text: assistantFile.basename, cls: 'assistant-suggestion-header' });
+		el.createEl('h6', { text: assistantFile.path, cls: 'assistant-suggestion-path' });
+	}
+
+	onChooseSuggestion(assistantFile: TFile, evt: MouseEvent | KeyboardEvent) {
+		if (this.chatWithNote) {
+			const activeFile = this.app.workspace.getActiveFile();
+			this.plugin.startNewChat(assistantFile, activeFile);
+		} else {
+			this.plugin.startNewChat(assistantFile, null);
+		}
+	}
+}
